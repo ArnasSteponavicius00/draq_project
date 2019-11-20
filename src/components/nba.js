fb0345fec9302c345420dc91834796e0575ef5ec
ARNAS STEPONAVICIUS <G00361891@gmit.ie>
2019-11-20 18:50:30 +0000

    use axios to get json of nba games

--- src/components/nba.js
+++ src/components/nba.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+
 
 class NBA extends React.Component{
-
-    state = {
-        games: []
+    constructor(props){
+        super(props);
+        this.state = {
+            games: []
+        };
     }
 
     componentDidMount(){
         axios.get('https://www.balldontlie.io/api/v1/games')
-        .then(res => {
-            this.setState({games: []});
+        .then(response => {
+            console.log(response);
+            this.setState({games: response.data.data});
+        })
+        .catch(function(error) {
+            console.log(error);
         })
     }
 
     render(){
-        return(
-            <div>
-                <h1>NBA</h1>
-                <ul>
-                {this.state.games.map(games => <li>{games}</li>)}
-                </ul>
+        const games = this.state.games.map((games, key)=>{
+            return <div key={key}>
+            <p>{games.home_team.full_name} vs. {games.visitor_team.full_name}</p>
             </div>
+        });
+
+        return(
+            <Card style={{width: '40rem', alignItems:'center', margin:'20px'}} 
+            border="success" bg="dark" className="text-center">
+                <div>
+                    <ul>
+                        {games}
+                    </ul>
+                </div>
+            </Card>
         );
     }
 }
