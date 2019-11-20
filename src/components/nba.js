9da73dd71ed984ca74f72284107c5235afbf8ecb
ARNAS STEPONAVICIUS <G00361891@gmit.ie>
2019-11-20 19:23:24 +0000

    add in api to get teams in league, add styling to nba page

--- src/components/nba.js
+++ src/components/nba.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 
 class NBA extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            games: []
+            games: [],
+            teams: []
         };
     }
 
@@ -20,24 +24,67 @@ class NBA extends React.Component{
         .catch(function(error) {
             console.log(error);
         })
+
+        axios.get('https://www.balldontlie.io/api/v1/teams')
+        .then(response => {
+            console.log(response);
+            this.setState({teams: response.data.data});
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
     }
 
     render(){
         const games = this.state.games.map((games, key)=>{
             return <div key={key}>
+            <Card style={{width: '20rem', alignItems:'center', margin:'20px'}} 
+                            border="success" bg="dark" className="text-center">
+            <p>{games.home_team_score} - {games.visitor_team_score}</p>
+            
             <p>{games.home_team.full_name} vs. {games.visitor_team.full_name}</p>
+            </Card>
+            </div>
+        });
+
+        const teams = this.state.teams.map((teams, key)=>{
+            return <div key={key}>
+            <Card style={{width: '20rem', alignItems:'center', margin:'20px'}} 
+                            border="danger" bg="dark" className="text-center">
+            <p>{teams.id} - {teams.abbreviation} - {teams.full_name} - {teams.division}</p>
+            </Card>
             </div>
         });
 
         return(
-            <Card style={{width: '40rem', alignItems:'center', margin:'20px'}} 
-            border="success" bg="dark" className="text-center">
-                <div>
-                    <ul>
-                        {games}
-                    </ul>
-                </div>
-            </Card>
+            <div className="liveGames">
+                <Container>
+                    <Row>
+                        <Col>
+                            <h2 className="live-games-header">Live Games</h2>
+                            <Card style={{width: '30rem', alignItems:'center', margin:'20px'}} 
+                            border="success" bg="dark" className="text-center">
+                                <div>
+                                    <ul>
+                                        {games}
+                                    </ul>
+                                </div>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <h2 className="league-teams-header">League Teams</h2>
+                            <Card style={{width: '30rem', alignItems:'center', margin:'20px'}} 
+                            border="danger" bg="dark" className="text-center">
+                                <div>
+                                    <ul>
+                                        {teams}
+                                    </ul>
+                                </div>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
